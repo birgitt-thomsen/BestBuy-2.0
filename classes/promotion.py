bf49b858113e14def90ad086ee8216ff228bf72b
Birgitt Thomsen <birgitt.thomsen@t-online.de>
2026-06-26 23:14:28 +0200

    Create Promotion class and implement promotion types

---
+++ classes/promotion.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+
+
+class Promotion(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def apply_promotion(self, product, quantity):
+        pass
+
+
+class PercentDiscount(Promotion):
+    def __init__(self, name, percent):
+        super().__init__(name)
+        self.percent = percent
+
+    def apply_promotion(self, product, quantity):
+        full_price = product.price * quantity
+        return full_price * (1 - self.percent / 100)
+
+
+class SecondHalfPrice(Promotion):
+    def apply_promotion(self, product, quantity):
+        pairs = quantity // 2
+        singles = quantity % 2
+
+        return (
+            pairs * (product.price * 1.5)
+            + singles * product.price
+        )
+
+
+class ThirdOneFree(Promotion):
+    def apply_promotion(self, product, quantity):
+        free_items = quantity // 3
+        paid_items = quantity - free_items
+
+        return paid_items * product.price
